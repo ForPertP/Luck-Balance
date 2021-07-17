4d4273b8f56ea0fb31b7b6e75691fbe0ae45f2e4
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-07-18 01:16:12 +0900

    Update Luck-Balance.cpp

--- Luck-Balance.cpp
+++ Luck-Balance.cpp
@@ -1,1 +1,27 @@
+int luckBalance(int k, vector<vector<int>> contests)
+{
+    vector<int> imp;
+    int luck = 0;
 
+    for (size_t i = 0; i < contests.size(); ++i)
+    {
+        if (contests[i][1])
+        {
+            imp.push_back(contests[i][0]);
+            luck -= contests[i][0];
+        }
+        else
+        {
+            luck += contests[i][0];
+        }
+    }
+
+    sort(imp.begin(), imp.end(), greater<int>());
+
+    for (size_t i = 0; i < min(k, (int)imp.size()); ++i)
+    {
+       luck += 2 * imp[i];
+    }
+
+    return luck;
+}
